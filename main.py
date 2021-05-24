545654f4db6d960efa454a5d6cb9130f5faa1ba3
Geoffrey Ritchey <Geoffrey.Ritchey@gmail.com>
2021-05-24 11:19:26 -0500

    get files from nelnet

---
+++ main.py
@@ -0,0 +1,16 @@
+import pysftp
+import os
+import build
+
+
+if __name__ == '__main__':
+    with pysftp.Connection(build.hostname, username=build.username, private_key_pass=build.private_key_pass) as sftp:
+        sftp.chdir('production/commerce_manager')
+        print(sftp.listdir('.'))
+        sftp.get_d(remotedir='.', localdir='.', preserve_mtime=True)
+        for filename in os.listdir():
+            if filename.startswith("lcu_"):
+                if sftp.exists(filename):
+                    sftp.remove(filename)
+                print(filename)
+
